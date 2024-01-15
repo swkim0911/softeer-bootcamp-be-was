5ccf19cc162aa22a31c5574de38adf1a06216c5c
swkim0911 <kimsungwon364@gmail.com>
2024-01-15 21:28:36 +0900

    feat: change Java Thread to Concurrent package

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -12,7 +12,7 @@ import org.slf4j.LoggerFactory;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
-    private static final String ROOT = "/Users/user/Documents/softeer/softeer-bootcamp-be-was/";
+    private static final String ROOT = "src/main/resources";
 
     private Socket connection;
 
@@ -26,41 +26,22 @@ public class RequestHandler implements Runnable {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
-            // 요청 헤더 읽기;
+            // 요청 헤더 읽기
             String requestPath = readRequestPath(in);
-            logger.debug("requestPath = {}", requestPath);
+            String fileType = getFileType(requestPath);
             DataOutputStream dos = new DataOutputStream(out);
-            if (requestPath.equals("/index.html")) {
-                byte[] body = readHtmlFile("src/main/resources/templates/index.html");
+            if (fileType.equals("html")) {
+                // 함수 묶기
+                byte[] body = readHtmlFile("/templates/index.html");
                 response200Header(dos, body.length, "html");
                 responseBody(dos, body);
-            } else if (requestPath.equals("/css/bootstrap.min.css")) {
-                byte[] body = readHtmlFile("src/main/resources/static/css/bootstrap.min.css");
-                response200Header(dos, body.length, "css");
-                responseBody(dos, body);
-            } else if (requestPath.equals("/css/styles.css")) {
-                byte[] body = readHtmlFile("src/main/resources/static/css/styles.css");
-                response200Header(dos, body.length, "css");
-                responseBody(dos, body);
-            }else if (requestPath.equals("/js/jquery-2.2.0.min.js")) {
-                byte[] body = readHtmlFile("src/main/resources/static/js/jquery-2.2.0.min.js");
-                response200Header(dos, body.length, "js");
-                responseBody(dos, body);
-            }else if (requestPath.equals("/js/bootstrap.min.js")) {
-                byte[] body = readHtmlFile("src/main/resources/static/js/bootstrap.min.js");
-                response200Header(dos, body.length, "js");
-                responseBody(dos, body);
-            }else if (requestPath.equals("/js/scripts.js")) {
-                byte[] body = readHtmlFile("src/main/resources/static/js/scripts.js");
-                response200Header(dos, body.length, "js");
-                responseBody(dos, body);
-            }else if (requestPath.equals("/favicon.ico")) {
-                byte[] body = readHtmlFile("src/main/resources/static/favicon.ico");
-                response200Header(dos, body.length, "ico");
+            }else{
+                byte[] body = readHtmlFile("/static" + requestPath);
+                response200Header(dos, body.length, fileType);
                 responseBody(dos, body);
             }
 
-            String bodyMessage = "hello world"; // 이렇게 하면 css
+            String bodyMessage = "hello world";
             byte[] body = bodyMessage.getBytes();
             response200Header(dos, body.length, "html");
             responseBody(dos, body);
@@ -70,17 +51,26 @@ public class RequestHandler implements Runnable {
         }
     }
 
+    private static String getFileType(String requestPath) {
+        int idx = requestPath.lastIndexOf(".");
+        return requestPath.substring(idx + 1);
+    }
+
     private static String readRequestPath(InputStream in) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
         String line = br.readLine();
-        logger.debug("request line : {}", line);
+        logger.debug("request line: {}", line);
         String requestPath = line.split(" ")[1];
         while (!line.isEmpty()) { //마지막 라인 = ""(empty)
             line = br.readLine();
-            logger.debug("header : {}", line);
+            logger.debug("header: {}", line);
         }
         return requestPath;
     }
+
+
+
+
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent, String type) {
         try {
@@ -93,6 +83,8 @@ public class RequestHandler implements Runnable {
                 dos.writeBytes("Content-Type: application/javascript;charset=utf-8\r\n");
             }else if (type.equals("ico")) {
                 dos.writeBytes("Content-Type: image/x-icon;charset=utf-8\r\n");
+            } else if (type.equals("woff")) {
+                dos.writeBytes("Content-Type: application/font-woff;charset=utf-8\r\n");
             }
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
             dos.writeBytes("\r\n");
@@ -110,7 +102,7 @@ public class RequestHandler implements Runnable {
         }
     }
     private byte[] readHtmlFile(String fileName) throws IOException {
-        // index.html 파일을 읽어서 문자열로 반환
-        return Files.readAllBytes(new File(fileName).toPath());
+        // index.html 파일을 읽어서 바이트 배열로 반환
+        return Files.readAllBytes(new File(ROOT+fileName).toPath());
     }
 }
