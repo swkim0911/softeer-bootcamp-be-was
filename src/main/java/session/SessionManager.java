b6b428d2790dfccbaac8f384f814264cba991e8a
swkim0911 <kimsungwon364@gmail.com>
2024-01-23 14:59:31 +0900

    feat: SessionManager 클래스 generateSessionId 함수 추가
    세션 아이디에 중복을 피하기 위해 UUID.randomUUID() 함수 사용

---
+++ src/main/java/session/SessionManager.java
@@ -0,0 +1,20 @@
+package session;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class SessionManager {
+
+	private static Map<String, String> sessionIdMap = new ConcurrentHashMap<>();
+
+	public static String generateSessionId(String userId) {
+		// UUID 난수를 통해 sessionId 생성
+		String sessionId = UUID.randomUUID().toString();
+		sessionIdMap.put(sessionId, userId);
+
+		return sessionId;
+	}
+
+}
