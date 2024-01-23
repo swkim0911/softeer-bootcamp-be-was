09c02924603e9a8076e0dd7df36d2e17b3979af1
swkim0911 <kimsungwon364@gmail.com>
2024-01-23 10:40:44 +0900

    feat: UserRequestHanlder /user/login uri 조회 로직 추가
    클라이언트가 로그인 요청시 /user/login uri로 요청해서 if 문으로 처리

--- src/main/java/handler/UserRequestHandler.java
+++ src/main/java/handler/UserRequestHandler.java
@@ -22,6 +22,11 @@ public class UserRequestHandler implements RequestHandler{
 			// index.html 으로 리다이렉트
 			return HttpResponseUtils.get302HttpResponse(HttpStatusCode.FOUND, version);
         }
-        return getHttpResponse(version, uri);
+		if (uri.equals("/user/login")) {
+			Map<String, String> parameters = QueryStringParser.getParameters(httpRequest.getBody());
+			//todo 사용자 아이디 비교해서 로그인 가능 여부 확인
+		}
+
+		return getHttpResponse(version, uri);
     }
 }
